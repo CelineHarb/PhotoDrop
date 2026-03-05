e1d23403817b43ccb9d067d8f59827f7c363dbf0
CelineHarb <celinesameh@hotmail.com>
2026-03-05 15:07:32 -0500

    started connect to drive logic

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
 using PhotoDrop.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Authentication + Google OAuth (store who is signed in)
+builder.Services.AddAuthentication( options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    //wires app to Google OAuth using our credentials in user secrets
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+
+    //Permission we're requesting from Google
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+    options.Scope.Add("https://www.googleapis.com/auth/drive.file"); // allows app to create/manage files it creates in the user's Drive
+
+    //after OAuth finishes, store access_token, refresh_token, expires_at, somewhere I can retrieve later 
+    options.SaveTokens = true;
+
+    // Helps ensure you get a refresh token for later uploads when host is not online
+    options.AccessType = "offline";
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -20,6 +51,31 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+// auth middleware 
+app.UseAuthentication(); // read the cookie on every request 
+app.UseAuthorization(); //enforce access rules
+
+// endpoints 
+app.MapGet("/auth/google/start", (HttpContext ctx, string eventName) =>
+{
+    var props = new AuthenticationProperties
+    {
+        RedirectUri = "/auth/google/finish"
+    };
+
+    props.Items["eventName"] = eventName; // temporary, for later use
+    return Results.Challenge(props, new[] { GoogleDefaults.AuthenticationScheme }); // starts the google oauth flow
+});
+
+app.MapGet("/auth/google/finish", async (HttpContext ctx) =>
+{
+    var accessToken = await ctx.GetTokenAsync("access_token");
+    if (string.IsNullOrWhiteSpace(accessToken))
+        return Results.Redirect("/get-started?error=missing_access_token");
+
+    return Results.Redirect("/get-started?connected=1");
+});
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
